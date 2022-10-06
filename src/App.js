810e92a30462269f9653dc8ed538ce4587e6ebc9
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2022-10-06 11:44:37 -0500

    improved learning goals

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn To Poop
+          Learn To Poop Better
         </a>
       </header>
     </div>
