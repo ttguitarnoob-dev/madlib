4b3c34f139496a2cc5af8800f0f8638a0cd812bb
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2022-10-06 16:34:41 -0500

    Set up routes and started setting up the create story page

--- src/components/CreateStory.jsx
+++ src/components/CreateStory.jsx
@@ -1,8 +1,18 @@
+import { storyOne } from "../data/StoryOne"
+import { useState } from "react"
+
+
 const CreateStory = () => {
+    // input field with onChange > run a function that changes state to the input (this.setAnswers maybe?) and then increments the step to load the next question
+    const [answers, setAnswers] = useState([])
+    let step = 0
 
     return (
         <div>
             <h1>Hello, CreateStory</h1>
+            <p>{storyOne}</p>
+            <p>{step}</p>
+            <p>{answers}</p>
 
 
         </div>
---
+++ src/data/StoryOne.js
@@ -0,0 +1,1 @@
+export const storyOne = ['hello', 2, 'smell']
