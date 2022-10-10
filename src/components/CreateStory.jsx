0c30220a62c23451a56a52cd5f99b181b5eaccf8
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-10-10 08:48:44 -0500

    not sure what changes cuz before weekend oops

--- src/components/CreateStory.jsx
+++ src/components/CreateStory.jsx
@@ -8,6 +8,7 @@ class CreateStory extends React.Component {
     constructor(props) {
         super(props);
         this.state = {value: ''};
+        
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
