7be82587f9438dbccb41bcacd368f36fdabee1dd
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-10-11 08:30:21 -0500

    user input updating state array now

--- src/components/CreateStory.jsx
+++ src/components/CreateStory.jsx
@@ -3,58 +3,81 @@ import { useState } from "react"
 import React from "react";
 
 
-class CreateStory extends React.Component {
-    // input field with onChange > run a function that changes state to the input (this.setAnswers maybe?) and then increments the step to load the next question
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event) {
-        this.setState({value: event.target.value})
-    }
-    handleSubmit(event) {
-        alert("Submitment of Poo in the form of the best type of " + this.state.value)
+const CreateStory = () => {
+    const [answers, setAnswers] = useState([])
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('event?', event.target[0].value)
+        setAnswers([...answers, event.target[0].value])
+        console.log('stat', answers)
     }
 
-    render() {
-        return (
-            <div>
+
+    return (
+        <div>
             <h1>Hello</h1>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <label>Enter Noun</label>
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <input type="text" />
                 <input type="submit" value="Submit" />
             </form>
-            </div>
-        )
-    }
-    // const [answers, setAnswers] = useState([])
-    // let step = 0
-    // const userSubmit = (event) => {
-        
-    //     console.log("submitment")
-    // }
-
-    // return (
-    //     <div>
-    //         <h1>Hello, CreateStory</h1>
-    //         <p>{storyOne[2]}</p>
-    //         <p>{step}</p>
-    //         <p>{answers}</p>
-    //         <div>
-    //             <form>
-    //                 <label htmlFor="answers">Enter A Noun</label>
-    //                 <input type="text" onSubmit={userSubmit} name="answers" id="answers" />
-    //                 <input type="submit" value="Submit" />
-    //             </form>
-    //         </div>
-
-
-    //     </div>
-    // )
+        </div>
+    )
 }
+
+// class CreateStory extends React.Component {
+//     // input field with onChange > run a function that changes state to the input (this.setAnswers maybe?) and then increments the step to load the next question
+//     constructor(props) {
+//         super(props);
+//         this.state = {value: ''};
+
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//     handleChange(event) {
+//         this.setState({value: event.target.value})
+//     }
+//     handleSubmit(event) {
+//         alert("Submitment of Poo in the form of the best type of " + this.state.value)
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//             <h1>Hello</h1>
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>Enter Noun</label>
+//                 <input type="text" value={this.state.value} onChange={this.handleChange} />
+//                 <input type="submit" value="Submit" />
+//             </form>
+//             </div>
+//         )
+//     }
+// const [answers, setAnswers] = useState([])
+// let step = 0
+// const userSubmit = (event) => {
+
+//     console.log("submitment")
+// }
+
+// return (
+//     <div>
+//         <h1>Hello, CreateStory</h1>
+//         <p>{storyOne[2]}</p>
+//         <p>{step}</p>
+//         <p>{answers}</p>
+//         <div>
+//             <form>
+//                 <label htmlFor="answers">Enter A Noun</label>
+//                 <input type="text" onSubmit={userSubmit} name="answers" id="answers" />
+//                 <input type="submit" value="Submit" />
+//             </form>
+//         </div>
+
+
+//     </div>
+// )
+// }
 
 export default CreateStory
